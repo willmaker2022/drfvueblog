5d801b193a3c6b75dc37be94f518cfacab5a05b4
ghh <gao_hh@163.com>
2022-04-22 15:30:20 +0800

    ui redesign

--- drfvueblog/settings.py
+++ drfvueblog/settings.py
@@ -72,8 +72,8 @@ ROOT_URLCONF = 'drfvueblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
-        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [os.path.join(BASE_DIR, 'frontend')],
 
         'APP_DIRS': True,
         'OPTIONS': {
